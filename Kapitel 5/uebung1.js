c7c074cae78c6c8a53d89740889e1a83b786b228
Marc Wenning-Künne <marcwenningkuenne@gmail.com>
2022-05-13 08:50:37 +0200

    Kapitel 5 - Übung 1 - Aufgabe 1+2

---
+++ Kapitel 5/uebung1.js
@@ -0,0 +1,10 @@
+"use strict";
+
+//Übung 1 - externes Skript
+//document.getElementById("absatz1").innerHTML = "Neuer Inhalt!";
+
+
+//Übung 1 - externes Skript + Event Handler
+function neuerInhalt(){
+    document.getElementById("absatz1").innerHTML = "Neuer Inhalt!";
+}
